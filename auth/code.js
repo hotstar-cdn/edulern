3dba17b38e324886c14297729d85dc60d8867c63
hotstar-cdn <dphcdn@gmail.com>
2025-06-07 12:47:52 +0530

    Update code.js

--- auth/code.js
+++ auth/code.js
@@ -1,7 +1,7 @@
 const redirectData = {
-  "abc123": "https://example.com/movie1",
-  "xyz789": "https://example.com/movie2",
-  "hotstar001": "https://disneyplus.com/movie-hotstar",
+  "MCI": "https://example.com/movie1",
+  "CPSIR": "https://example.com/movie2",
+  "": "https://disneyplus.com/movie-hotstar",
   "marvel42": "https://example.com/marvel-special",
   "disneyking": "https://example.com/disney-kingdom"
 };
