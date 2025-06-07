019cace809d44dac4a8cc4322d6b9c00b56c18bc
hotstar-cdn <dphcdn@gmail.com>
2025-06-07 12:42:02 +0530

    Create code.js

---
+++ auth/code.js
@@ -0,0 +1,7 @@
+const redirectData = {
+  "abc123": "https://example.com/movie1",
+  "xyz789": "https://example.com/movie2",
+  "hotstar001": "https://disneyplus.com/movie-hotstar",
+  "marvel42": "https://example.com/marvel-special",
+  "disneyking": "https://example.com/disney-kingdom"
+};
