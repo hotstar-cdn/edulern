cfc728aa384454235b387abe298add54c93cf0f8
hotstar-cdn <dphcdn@gmail.com>
2025-06-23 13:15:39 +0530

    Update data.js

--- auth/data.js
+++ auth/data.js
@@ -13,16 +13,16 @@ const users = [
     role: "student"
   },
   {
-    formNo: "2001",
-    name: "Priya Gupta",
-    fathersName: "Ramesh Gupta",
-    mothersName: "Suman Gupta",
-    class: "11 JEE",
+    formNo: "AM01",
+    name: "Akshay Mangal",
+    fathersName: "N/A",
+    mothersName: "N/A",
+    class: "N/A",
     email: "priya@example.com",
     mobile: "9876543210",
-    profilePic: "https://randomuser.me/api/portraits/women/2.jpg",
-    password: "teach456",
-    role: "teacher"
+    profilePic: "https://instagram.fktu2-1.fna.fbcdn.net/v/t51.29350-15/474597363_1107030471206495_8600997027211797777_n.heic?stp=dst-jpg_e35_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6IkZFRUQuaW1hZ2VfdXJsZ2VuLjE0NDB4MTQ0MC5zZHIuZjI5MzUwLmRlZmF1bHRfaW1hZ2UifQ&_nc_ht=instagram.fktu2-1.fna.fbcdn.net&_nc_cat=111&_nc_oc=Q6cZ2QEAfVP29NGScW_O6y9cgoRCa4v0x8kC5fta-Pb95cSa2YWlMyUZQddMlUhAYwF_TOc&_nc_ohc=_znMCeQEgpwQ7kNvwHfn8h6&_nc_gid=px4nPgzrmpPbiRlxYXAbdA&edm=AP4sbd4BAAAA&ccb=7-5&ig_cache_key=MzU0OTgyNTkxODY2OTk1NzYwMA%3D%3D.3-ccb7-5&oh=00_AfPWjOlFJvTAQZPnupdGw2JEbyiB7wfU1OTFXH74kpiHSA&oe=685EE65E&_nc_sid=7a9f4b",
+    password: "mci@mci.mci",
+    role: "Admin"
   }
   // Add more users as needed
 ];
